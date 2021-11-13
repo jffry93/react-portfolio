c841976633607d5ecb6196af6d28d6c62522c8b4
Jeffrey Zalischi <jay.zalischi@gmail.com>
2021-11-12 22:22:15 -0500

    added animation to contact page

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,19 +1,74 @@
 //PAGE ANIMATION
 import { motion } from 'framer-motion';
-import { pageAnimation } from '../animation';
+import { pageAnimation, titleAnimation } from '../animation';
+import styled from 'styled-components';
 
 const ContactUs = () => {
   return (
-    <motion.div
+    <StyledContactStyle
       variants={pageAnimation}
       initial='hidden'
       animate='show'
       exit='exit'
       style={{ background: '#fff' }}
     >
-      <h1>Contact</h1>
-    </motion.div>
+      <StyledTitle>
+        <StyledHide>
+          <motion.h2 variants={titleAnimation}> Get in Touch</motion.h2>
+        </StyledHide>
+      </StyledTitle>
+      <div>
+        <StyledHide>
+          <StyledSocial variants={titleAnimation}>
+            <StyledCircle></StyledCircle>
+            <h2>Send us a message</h2>
+          </StyledSocial>
+        </StyledHide>
+        <StyledHide>
+          <StyledSocial variants={titleAnimation}>
+            <StyledCircle></StyledCircle>
+            <h2>Send us an email</h2>
+          </StyledSocial>
+        </StyledHide>
+        <StyledHide>
+          <StyledSocial variants={titleAnimation}>
+            <StyledCircle></StyledCircle>
+            <h2>Social Media</h2>
+          </StyledSocial>
+        </StyledHide>
+      </div>
+    </StyledContactStyle>
   );
 };
+
+const StyledContactStyle = styled(motion.div)`
+  padding: 5rem 10rem;
+  color: #353535;
+  min-height: 90vh;
+`;
+
+const StyledTitle = styled.div`
+  margin-bottom: 4rem;
+  color: black;
+`;
+
+const StyledHide = styled.div`
+  overflow: hidden;
+`;
+
+const StyledCircle = styled.div`
+  border-radius: 50%;
+  width: 3rem;
+  height: 3rem;
+  background: #353535;
+`;
+
+const StyledSocial = styled(motion.div)`
+  display: flex;
+  align-items: center;
+  h2 {
+    margin: 2rem;
+  }
+`;
 
 export default ContactUs;
